57375c0f265649ef586964556d1e5acf24c7f54e
Bason <deniskornilovdrummer@gmail.ru>
2017-11-09 14:50:40 +0300

    check

--- _1/src/main/java/main.java
+++ _1/src/main/java/main.java
@@ -1,12 +1,41 @@
-public class main {
+import org.telegram.telegrambots.*;
+import org.telegram.telegrambots.api.objects.CallbackQuery;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
 
-    public static void main(String[] args)
-    {
-        int a = 10;
-        int b = 1;
-        int x = 0;
+public class main extends TelegramLongPollingBot {
 
-        System.out.println(a);
+    public static void main(String[] args) {
+        // TelegramBot bot = new TelegramBot("369166204:AAFfXhL-blM-T4bJL_nC0h7v0a2yXs9kfeA");
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+        try {
+            telegramBotsApi.registerBot(new main());
+        } catch (TelegramApiException e){e.printStackTrace();
+        }
     }
 
+    public String getBotUsername(){
+        return "DenisKorn";
+    }
+
+    @Override
+    public String getBotToken(){
+        return "369166204:AAFfXhL-blM-T4bJL_nC0h7v0a2yXs9kfeA";
+    }
+
+    public void onUpdateReceived(Update update){
+        Message message;
+
+        message = update.getMessage();
+
+        String userText = message.getText();
+
+        System.out.println(userText);
+
+    }
 }
